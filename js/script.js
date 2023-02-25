5fb2e38d6cfa7ccacd6dd4d09af80cbef44ded89
Md Alve <mdalve1091@gmail.com>
2023-02-25 19:09:20 -0300

    Added select option for select country by region

--- js/script.js
+++ js/script.js
@@ -1,74 +1,96 @@
 // load api from rest country api
 loadCountry = () => {
-    fetch("https://restcountries.com/v3.1/all")
-        .then((res) => res.json())
-        .then((data) => {
-            displayCountry(data.slice(0, 6));
-        });
+  fetch("https://restcountries.com/v3.1/all")
+    .then((res) => res.json())
+    .then((data) => {
+      displayCountry(data.slice(0, 6));
+    });
 };
-// show all country 
+// show all country
 showMoreData = () => {
-    fetch("https://restcountries.com/v3.1/all")
-        .then((res) => res.json())
-        .then((data) => {
-            displayCountry(data);
-        });
-    let showMoreData = document.getElementById("showMoreData");
-    let showLessData = document.getElementById('showLessData')
-    showMoreData.style.display = 'none'
-    showLessData.removeAttribute('style')
-}
+  fetch("https://restcountries.com/v3.1/all")
+    .then((res) => res.json())
+    .then((data) => {
+      displayCountry(data);
+    });
+  let showMoreData = document.getElementById("showMoreData");
+  showMoreData.style.display = "none";
+};
 
 // display all country in web page
 displayCountry = (countries) => {
-    let mainContainer = document.getElementById("mainContainer");
-    countries.forEach((country) => {
-        // console.log(country.cca2);
-        let countryCard = document.createElement("div");
-        countryCard.classList.add(
-            "card",
-            "card-compact",
-            "w-96",
-            "bg-base-100",
-            "shadow-xl",
-            "my-4"
-        );
-        countryCard.innerHTML = `
+  let mainContainer = document.getElementById("mainContainer");
+  mainContainer.innerHTML = ''
+  countries.forEach((country) => {
+    // console.log(country.cca2);
+    let countryCard = document.createElement("div");
+    countryCard.classList.add(
+      "card",
+      "card-compact",
+      "w-96",
+      "bg-base-100",
+      "shadow-xl",
+      "my-4"
+    );
+    countryCard.innerHTML = `
         <figure><img src="${country.flags.png}" alt="Shoes" /></figure>
         <div class="card-body">
           <h2 class="card-title text-3xl font-medium">${country.name.common
-            }</h2>
+      }</h2>
           <p class ="text-2xl font-light">Capital:- ${country.capital ? country.capital[0] : "No Capital"
-            }</p>
+      }</p>
           <div class="card-actions justify-end">
             <label for="my-modal" class="btn btn-primary" onclick="loadId('${country.cca2
-            }')">Detail</label>
+      }')">Detail</label>
           </div>
         </div>
         `;
-        mainContainer.appendChild(countryCard);
-    });
-
+    mainContainer.appendChild(countryCard);
+  });
 };
 
+// display country filter by region
+const selectElement = document.getElementById('select')
+selectElement.addEventListener('change', (event) => {
+  const selectedOptionValue = event.target.value;
+  let url;
+  if (selectedOptionValue === 'allCountry') {
+    url = "https://restcountries.com/v3.1/all"
+  } else {
+    url = `https://restcountries.com/v3.1/region/${selectedOptionValue}`
+  }
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayCountry(data));
+});
 // call loadCountry function
 loadCountry();
 
 // load country details
 let loadId = (countryCode) => {
-    let url = `https://restcountries.com/v3.1/alpha/${countryCode}`;
-    fetch(url)
-        .then((res) => res.json())
-        .then((data) => displayCountryDetail(data[0]));
+  let url = `https://restcountries.com/v3.1/alpha/${countryCode}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayCountryDetail(data[0]));
 };
 
 // display country details with modal
 displayCountryDetail = (detail) => {
-    let modalBox = document.getElementById('modalBox')
-    modalBox.innerHTML = ''; // clear previous content
-    let modalDiv = document.createElement('div')
-    let { name, flags, cca2, capital, region, area, maps, population, continents } = detail
-    modalDiv.innerHTML = `
+  let modalBox = document.getElementById("modalBox");
+  modalBox.innerHTML = ""; // clear previous content
+  let modalDiv = document.createElement("div");
+  let {
+    name,
+    flags,
+    cca2,
+    capital,
+    region,
+    area,
+    maps,
+    population,
+    continents,
+  } = detail;
+  modalDiv.innerHTML = `
         <h1 class="text-2xl font-bold my-2">Name:- ${name.common}</h1>
         <img class="w-72" src="${flags.png}" alt="">
         <p class="text-xl">Continents:- ${continents[0]}</p>
@@ -81,8 +103,6 @@ displayCountryDetail = (detail) => {
         <div class="modal-action">
             <label for="my-modal" class="btn">Close</label>
         </div>
-    `
-    modalBox.appendChild(modalDiv)
-}
-
-
+    `;
+  modalBox.appendChild(modalDiv);
+};
